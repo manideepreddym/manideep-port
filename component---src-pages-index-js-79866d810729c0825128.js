6906849670b02818e61bc0b488d1416dae59da40
Manideep Reddy <manideepreddy966@gmail.com>
2025-01-24 12:51:15 -0800

    Updates

---
+++ component---src-pages-index-js-79866d810729c0825128.js
@@ -0,0 +1,34 @@
+'use strict';
+(self.webpackChunkmanideep_port = self.webpackChunkmanideep_port || []).push([
+  [293],
+  {
+    9639: function (e, t, l) {
+      l.r(t);
+      var n = l(6540),
+        a = l(4612),
+        c = l(7430);
+      const r = a.default.main.withConfig({
+        displayName: 'pages__StyledMainContainer',
+        componentId: 'sc-vjke8o-0',
+      })(['counter-reset:section;']);
+      t.default = e => {
+        let { location: t } = e;
+        return n.createElement(
+          c.PE,
+          { location: t },
+          n.createElement(
+            r,
+            { className: 'fillHeight' },
+            n.createElement(c.lq, null),
+            n.createElement(c.Im, null),
+            n.createElement(c.XP, null),
+            n.createElement(c.Hi, null),
+            n.createElement(c.Jt, null),
+            n.createElement(c.Bv, null),
+          ),
+        );
+      };
+    },
+  },
+]);
+//# sourceMappingURL=component---src-pages-index-js-79866d810729c0825128.js.map
