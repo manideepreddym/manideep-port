7035ecc05d26192078d7b1b7b0f09b28740ee798
Manideep Reddy <manideepreddy966@gmail.com>
2025-01-25 10:17:09 -0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-56b0f430005ff7c3cdd6.js"
+    "url": "webpack-runtime-08cd00364c5e1a00c0cc.js"
   },
   {
     "url": "framework-8f58a5cbe3f4b3ef31af.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "900ed22e0aa92ff3de189f53838dde88"
+    "revision": "fba6f27c8339799931f62fd957d0a379"
   },
   {
     "url": "manifest.webmanifest",
